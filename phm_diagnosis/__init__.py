9ed71cd6ddc33d4cf69414e5bb38e6bf32657041
2364839934@qq.com <2364839934@qq.com>
2022-10-26 11:25:22 +0800

    add feature part

--- phm_diagnosis/__init__.py
+++ phm_diagnosis/__init__.py
@@ -5,3 +5,14 @@ from .causal import causal_model_build
 from .causal import plot_causal_graph
 from .causal import causal_discovery
 
+from .phm_feature import enable_parallel
+from .phm_feature import disable_parallel
+from .phm_feature import feature_t
+#from .phm_feature import feature_f
+from .phm_feature import fft
+from .phm_feature import power
+from .phm_feature import ifft
+from .phm_feature import cepstrum
+from .phm_feature import envelope
+from .phm_feature import window
+from .phm_feature import divide
